72c1ccacb6c1598003c77e046fe8f7db8270bb62
Ravi <ravi.k@imsolutions.mobi>
2025-07-01 18:04:20 +0530

    commit

--- src/app/components/Amenities.tsx
+++ src/app/components/Amenities.tsx
@@ -4,31 +4,32 @@ import Image from 'next/image';
 import styles from '../../public/css/BiodiversityHighlights.module.css';
 
 const images = [
-    '/images/Badminton Court.webp',
-    '/images/Basket Ball Court.webp',
-    '/images/Club-Entrance-View-min_Binary.webp',
-    '/images/Indoor Gym.webp',
-    '/images/Jogging Track .webp',
-    '/images/Swimming.webp',
-    '/images/Table Tennis.webp',
-    '/images/Yoga.webp',
+    { src: '/images/Badminton Court.webp', title: 'Badminton Court' },
+    { src: '/images/Basket Ball Court.webp', title: 'Basket Ball Court' },
+    { src: '/images/Club-Entrance-View-min_Binary.webp', title: 'Club Entrance' },
+    { src: '/images/Indoor Gym.webp', title: 'Indoor Gym' },
+    { src: '/images/Jogging Track .webp', title: 'Jogging Track' },
+    { src: '/images/Swimming.webp', title: 'Swimming Pool' },
+    { src: '/images/Table Tennis.webp', title: 'Table Tennis' },
+    { src: '/images/Yoga.webp', title: 'Yoga' },
 ];
 
 export default function Amenities() {
     return (
         <section className={styles.section}>
             <div className={styles.marquee}>
-                <div className={styles.marqueeContent}>
-                    {images.concat(images).map((src, idx) => (
+                <div className={styles.marqueeContent} >
+                    {images.concat(images).map((img, idx) => (
                         <div key={idx} className={styles.imageWrapper}>
                             <Image
-                                src={src}
-                                alt=""
+                                src={img.src}
+                                alt={img.title}
                                 width={300}
                                 height={200}
                                 loading="lazy"
                                 className={styles.image}
                             />
+                            <p className={styles.imageTitle}>{img.title}</p>
                         </div>
                     ))}
                 </div>
